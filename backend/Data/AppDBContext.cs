0ef7b72ffd4accbe1b4293b6ec0fe419ecad2067
eduardo-lopes-95 <eduardosabino@ymail.com>
2022-04-05 13:57:22 -0300

    subindo pasta backend

---
+++ backend/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Parceria.Data
+{
+    internal sealed class AppDBContext : DbContext
+    {
+        public DbSet<Convite> Convites { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlServer("Data Source=./Data/AppDB.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Convite>().ToTable(("Parcerias"));
+        }
+    }
+}
---
+++ backend/Data/Convite.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parceria.Data
+{
+    public class Convite
+    {
+        [Key]
+        public int ConviteId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string EmailDestinatario { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
+        public string WalletRemetente { get; set; }
+
+        public string Link { get; set; } = string.Empty;
+
+    }
+}
